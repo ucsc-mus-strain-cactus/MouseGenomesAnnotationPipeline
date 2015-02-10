bdf21c00d1d681e7b47e7f9d23af797c961690db
Joel Armstrong <jcarmstr@ucsc.edu>
2015-02-09 17:29:54 -0800

    Script to replicate the 1412 alignment

--- bin/download_1412_assemblies.sh
+++ bin/download_1412_assemblies.sh
@@ -5,19 +5,19 @@ set -o pipefail
 set -o nounset
 
 # a bit weird, but we get the absolute path of the pipeline src dir.
-PIPELINE_DIR=$(readlink -f $(dirname $0)/..)
+PIPELINE_DIR=$(readlink -f "$(dirname "$0")/..")
 # Read the config file containing MSCA_DATA_DIR variable, etc.
-source ${PIPELINE_DIR}/etc/pipeline_vars.sh
+. "${PIPELINE_DIR}/etc/pipeline_vars.sh"
 
-mkdir -p ${MSCA_DATA_DIR}/assemblies/1412/genome/
-cd ${MSCA_DATA_DIR}/assemblies/1412/genome/
+mkdir -p "${MSCA_DATA_DIR}/assemblies/1412/genome/"
+cd "${MSCA_DATA_DIR}/assemblies/1412/genome/"
 echo "Downloading assemblies. Grab a cup of coffee."
 wget ftp://ftp-mouse.sanger.ac.uk/REL-1412-Assembly/*.masked.gz*
 
 echo "Verifying file integrity."
-md5sum -c *.gz.md5
+md5sum -c ./*.gz.md5
 
 echo "Extracting files."
-gunzip *.gz
+gunzip ./*.gz
 
 echo "Done."
--- etc/pipeline_vars.sh
+++ etc/pipeline_vars.sh
@@ -4,4 +4,4 @@
 export MSCA_DATA_DIR=/hive/groups/recon/projs/mus_strain_cactus/pipeline_data
 
 # Batch system parameters for progressiveCactus. Adjust to suit your setup.
-export PROGRESSIVE_CACTUS_BATCH_ARGS="--batchSystem parasol --bigBatchSystem singleMachine --defaultMemory 8589934593 --bigMemoryThreshold 8589934592 --bigMaxMemory 893353197568 --bigMaxCpus 25 --maxThreads 25 --parasolCommand='/cluster/home/jcarmstr/bin/parasol -host=ku' --retryCount 3"
+export PROGRESSIVE_CACTUS_BATCH_ARGS="--batchSystem parasol --bigBatchSystem singleMachine --defaultMemory 8589934593 --bigMemoryThreshold 8589934592 --bigMaxMemory 893353197568 --bigMaxCpus 30 --maxThreads 30 --parasolCommand='/cluster/home/jcarmstr/bin/parasol -host=ku' --retryCount 3"
