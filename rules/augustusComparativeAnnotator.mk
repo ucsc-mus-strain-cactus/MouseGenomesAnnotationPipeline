d2126e15992b0edb5ce8b3cdfb4998bd77953076
ifiddes <ian.t.fiddes@gmail.com>
2015-08-27 19:30:27 -0700

    oops

--- rules/augustusComparativeAnnotator.mk
+++ rules/augustusComparativeAnnotator.mk
@@ -7,19 +7,19 @@ maxCpus = 1024
 defaultMemory = 8589934592
 maxJobDuration = 36000
 
-gencodeSubset = ${gencodeComp}
 comparativeAnnotationDir = ${ANNOTATION_DIR}_Augustus
-transMapChainedAllPsls = ${augustusOrgs:%=${TRANS_MAP_DIR}/transMap/%/transMap${gencodeSubset}.psl}
-transMapEvalAllGp = ${augustusOrgs:%=${TRANS_MAP_DIR}/transMap/%/transMap${gencodeSubset}.gp}
+transMapChainedAllPsls = ${augustusOrgs:%=${TRANS_MAP_DIR}/transMap/%/transMap${gencodeComp}.psl}
+transMapEvalAllGp = ${augustusOrgs:%=${TRANS_MAP_DIR}/transMap/%/transMap${gencodeComp}.gp}
 augGps = ${augustusOrgs:%=${TMR_DIR}/%.gp}
-srcGp = ${SRC_GENCODE_DATA_DIR}/wgEncode${gencodeSubset}.gp
-srcFa = ${SRC_GENCODE_DATA_DIR}/wgEncode${gencodeSubset}.fa
+compGp = ${SRC_GENCODE_DATA_DIR}/wgEncode${gencodeComp}.gp
+basicGp = ${SRC_GENCODE_DATA_DIR}/wgEncode${gencodeBasic}.gp
+srcFa = ${SRC_GENCODE_DATA_DIR}/wgEncode${gencodeComp}.fa
 TMR_DIR = /cluster/home/ifiddes/mus_strain_data/pipeline_data/comparative/1504/augustus/tmr
 augustusGps = ${augustusOrgs:%=${TMR_DIR}/%.gp}
-comparativeJobTreeDir = $(shell pwd)/.Augustus_${gencodeSubset}_${MSCA_VERSION}_comparativeAnnotatorJobTree
+comparativeJobTreeDir = $(shell pwd)/.Augustus_${gencodeComp}_${MSCA_VERSION}_comparativeAnnotatorJobTree
 alignJobTreeDir = $(shell pwd)/.consensusAlignJobTree
 alignLog = $(shell pwd)/augustusAlign.log
-log = $(shell pwd)/Augustus_${gencodeSubset}_${MSCA_VERSION}_jobTree.log
+log = $(shell pwd)/Augustus_${gencodeComp}_${MSCA_VERSION}_jobTree.log
 consensusDir = ${comparativeAnnotationDir}/consensus
 augustusStatsDir = ${comparativeAnnotationDir}/augustus_stats
 augustusBeds = ${augustusOrgs:%=${augustusStatsDir}/%.bed}
@@ -31,7 +31,7 @@ all: checkout ${comparativeAnnotationDir}/DONE consensus
 checkout:
 	cd ../comparativeAnnotator && git checkout augustus
 
-${comparativeAnnotationDir}/DONE: checkout ${srcGp} ${transMapChainedAllPsls} ${transMapEvalAllGp} ${augustusGps}
+${comparativeAnnotationDir}/DONE: checkout ${compGp} ${transMapChainedAllPsls} ${transMapEvalAllGp} ${augustusGps}
 	@mkdir -p $(dir $@)
 	rm -rf ${comparativeJobTreeDir}
 	if [ "${batchSystem}" = "parasol" ]; then \
@@ -40,13 +40,13 @@ ${comparativeAnnotationDir}/DONE: checkout ${srcGp} ${transMapChainedAllPsls} ${
 		export PATH=./bin/:./sonLib/bin:./jobTree/bin:${PATH} && \
 		${python} src/annotationPipeline.py --refGenome ${srcOrg} --genomes ${augustusOrgs} --sizes ${targetChromSizes} \
 		--psls ${transMapChainedAllPsls} --gps ${transMapEvalAllGp} --fastas ${targetFastaFiles} --refFasta ${queryFasta} \
-		--annotationGp ${srcGp} --batchSystem ${batchSystem} --gencodeAttributeMap ${srcGencodeAttrsTsv} \
+		--annotationGp ${compGp} --batchSystem ${batchSystem} --gencodeAttributeMap ${srcAttrsTsv} \
 		--defaultMemory ${defaultMemory} --jobTree ${comparativeJobTreeDir} --maxJobDuration ${maxJobDuration} \
 		--maxThreads ${maxThreads} --stats --outDir ${comparativeAnnotationDir} --augustusGps ${augustusGps} &> ${log}" ;\
 	else \
 		${python} ../comparativeAnnotator/src/annotationPipeline.py --refGenome ${srcOrg} --genomes ${augustusOrgs} --sizes ${targetChromSizes} \
 		--psls ${transMapChainedAllPsls} --gps ${transMapEvalAllGp} --fastas ${targetFastaFiles} --refFasta ${queryFasta} \
-		--annotationGp ${srcGp} --batchSystem ${batchSystem} --gencodeAttributeMap ${srcGencodeAttrsTsv} \
+		--annotationGp ${compGp} --batchSystem ${batchSystem} --gencodeAttributeMap ${srcAttrsTsv} \
 		--defaultMemory ${defaultMemory} --jobTree ${comparativeJobTreeDir} --maxJobDuration ${maxJobDuration} \
 		--maxThreads ${maxThreads} --stats --outDir ${comparativeAnnotationDir} --augustusGps ${augustusGps} &> ${log} ;\
 	fi
@@ -94,5 +94,5 @@ ${comparativeAnnotationDir}/CONSENSUS_DONE: ${comparativeAnnotationDir}/AUG_ALIG
 	@mkdir -p $(dir $@)
 	cd ../comparativeAnnotator && ${python} scripts/consensus.py --genomes ${augustusOrgs} \
 	--compAnnPath ${comparativeAnnotationDir} --statsDir ${augustusStatsDir} --outDir ${consensusDir} \
-	--attributePath ${srcGencodeAttrsTsv} --augGps ${augGps} --tmGps ${transMapEvalAllGp}
+	--attributePath ${srcAttrsTsv} --augGps ${augGps} --tmGps ${transMapEvalAllGp} --compGp ${compGp} --basicGp ${basicGp}
 	touch $@
