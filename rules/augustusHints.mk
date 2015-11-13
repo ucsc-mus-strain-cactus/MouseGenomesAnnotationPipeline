5c3e4fd0626203707bc023ac543ca881b72e5850
ifiddes <ian.t.fiddes@gmail.com>
2015-11-13 10:38:37 -0800

    explicit hints dir

--- rules/augustusHints.mk
+++ rules/augustusHints.mk
@@ -1,20 +1,21 @@
 #####
-# Build an Augustus hints database. Expects a base folder to find bamfiles in.
-# which you pass as FOLDER=
-# TODO: make this work pan-genome. right now it is required you also pass mapTargetOrg=
+# Build an Augustus hints database.
+# Pass filterTissues=X and/or filterCenters=Y to filter out any BAMfiles whose path contains those strings
+# I.E. filterTissues=lung heart filterCenters=unc. This assumes you have a similar directory structure to what I use
+# genome/center/tissue/bamfiles
 #####
 include defs.mk
 
 
-all: ${augustusOrgs:%=%.runOrg} merge
+all: ${augustusOrgs:%=%.runOrg} finishDb
 
 clean: ${augustusOrgs:%=%.runOrgClean}
 
 %.runOrg:
-	${MAKE} -f rules/augustusHints.mk runOrg mapTargetOrg=$*
+    ${MAKE} -f rules/augustusHints.mk runOrg mapTargetOrg=$*
 
 %.runOrgClean:
-	${MAKE} -f rules/augustusHints.mk runOrgClean mapTargetOrg=$*
+    ${MAKE} -f rules/augustusHints.mk runOrgClean mapTargetOrg=$*
 
 
 ifneq (${mapTargetOrg},)
@@ -27,8 +28,8 @@ ifneq (${filterCenters},)
 fc = --filterCenters ${filterCenters}
 endif
 
- # TODO: make this the actual hints file in defs.mk
-db = ${AUGUSTUS_DIR}/hints_ian_build.db 
+hintsDir = ${AUGUSTUS_WORK_DIR}/hints
+
 fofn = ${AUGUSTUS_DIR}/rnaseq_fofn/${mapTargetOrg}
 
 fasta = ${ASM_GENOMES_DIR}/${mapTargetOrg}.fa
@@ -40,33 +41,33 @@ doneFlagDir = ${DONE_FLAG_DIR}/${mapTargetOrg}
 jobTreeTmpDir = $(shell pwd -P)/${jobTreeRootTmpDir}/augustus_db/${mapTargetOrg}
 jobTreeJobOutput = ${jobTreeTmpDir}/augustus_hints_db.out
 jobTreeJobDir = ${jobTreeTmpDir}/jobTree
-done = ${doneFlagDir}/hints_db.done
+done = ${doneFlagDir}/hintsDb.done
 
 runOrg: ${fofn} ${done}
 
 ${fofn}:
-	@mkdir -p $(dir $@)
-	find ${rnaSeqDataDir}/${mapTargetOrg} | grep bam$$ > $@
+    @mkdir -p $(dir $@)
+    find ${rnaSeqDataDir}/${mapTargetOrg} | grep bam$$ > $@
 
 ${done}:
-	@mkdir -p $(dir $@)
-	@mkdir -p ${jobTreeTmpDir}
-	cd ../comparativeAnnotator && ${python} augustus/build_hints_db.py ${jobTreeOpts} \
-	--genome ${mapTargetOrg} --bamFofn ${fofn} --fasta ${fasta} --database ${db} ${fc} ${ft} \
-	--jobTree ${jobTreeJobDir} &> ${jobTreeJobOutput}
-	touch $@
+    @mkdir -p $(dir $@)
+    @mkdir -p ${jobTreeTmpDir}
+    @mkdir -p ${hintsDir}
+    ${python} comparativeAnnotator/augustus/build_hints_db.py ${jobTreeOpts} \
+    --genome ${mapTargetOrg} --bamFofn ${fofn} --fasta ${fasta} --database ${hintsDb} ${fc} ${ft} \
+    --jobTree ${jobTreeJobDir} --hintsDir ${hintsDir} &> ${jobTreeJobOutput}
+    touch $@
 
 runOrgClean:
-	rm -rf ${fofn} ${done} ${jobTreeJobDir}
+    rm -rf ${fofn} ${done} ${jobTreeJobDir}
 
 else
 
-mergeDone = ${doneFlagDir}/merged_hints.done
+dbDone = ${DONE_FLAG_DIR}/indexedHints.done
 
-merge: ${mergeDone}
+finishDb: ${dbDone}
 
-${mergeDone}:
-	load2sqlitedb --dbaccess=${db} --makeIdx
-	touch $@
+${dbDone}:
+    load2sqlitedb --makeIdx --dbaccess ${hintsDb}
 
 endif
