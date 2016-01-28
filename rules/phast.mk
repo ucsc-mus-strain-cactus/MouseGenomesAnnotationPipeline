67ff542547b1596f30a5383fe1df2a461c99b5ce
ifiddes <ian.t.fiddes@gmail.com>
2016-01-28 08:08:44 -0800

    Makefile update to run dles script

--- rules/phast.mk
+++ rules/phast.mk
@@ -13,23 +13,22 @@ make_dots = $(subst ${comma},.,$1)
 # function to convert spaces to commas. $(eval) is a hack to get a space
 make_commas = $(subst $(eval) ,${comma},$1)
 
-PHAST_ANALYSIS_DIR=${DATA_DIR}/comparative/${VERSION}/phastAnalysis
-dlessOutDir = ${PHAST_ANALYSIS_DIR}/dless
-
 # if targetGenomes is defined on the command line, format it for dless
 ifneq (${targetGenomes},)
 targetGenomeStr = --targetGenomes ${targetGenomes}
-outputFileBase = (call make_dots,${targetGenomes})
+outputDirBase = (call make_dots,${targetGenomes})
 else
 targetGenomeCommas = $(call make_commas,${mappedOrgs})
-outputFileBase = $(call make_dots,${targetGenomeCommas})
+outputDirBase = $(call make_dots,${targetGenomeCommas})
 targetGenomeStr = 
 endif
 
-outputDlessGff = ${dlessOutDir}/${outputFileBase}.gff
-modFile=${PHAST_ANALYSIS_DIR}/${outputFileBase}.mod
-errorFile=${PHAST_ANALYSIS_DIR}/${outputFileBase}.err
-4dSitesBed=${PHAST_ANALYSIS_DIR}/4d.bed
+PHAST_ANALYSIS_DIR=${DATA_DIR}/comparative/${VERSION}/phastAnalysis/${outputDirBase}
+dlessOutDir = ${PHAST_ANALYSIS_DIR}/dless
+outputDlessGff = ${dlessOutDir}/dless.gff
+modFile=${PHAST_ANALYSIS_DIR}/rates.mod
+errorFile=${PHAST_ANALYSIS_DIR}/rates.err
+4dSitesBed=${PHAST_ANALYSIS_DIR}/4d_no_conserved.bed
 wigDir=${PHAST_ANALYSIS_DIR}/phyloPWigs
 
 # jobTree for dless
@@ -59,12 +58,13 @@ ${outputDlessGff}: ${modFile}
 # or underestimating the rate due to conserved nonsynonymous mutations.
 ${4dSitesBed}: ${cleanedCdsBed}
 	@mkdir -p $(dir $@)
-	hal4dExtract --conserved ${halFile} ${srcOrg} ${cleanedCdsBed} $@.${tmpExt}
+	hal4dExtract ${halFile} ${srcOrg} ${cleanedCdsBed} $@.${tmpExt}
 	mv -f $@.${tmpExt} $@
 
 # .mod extension required after tmpExt because phast will check the
 # output file extension for some reason
 ${modFile}: ${4dSitesBed}
 	@mkdir -p $(dir $@)
-	halPhyloPTrain.py --numProc 6 --noAncestors --no4d ${targetGenomeStr} ${halFile} ${srcOrg} ${4dSitesBed} $@.${tmpExt}.mod --error ${errorFile}
+	cd ../comparativeAnnotator && ${python} hal/phyloP/halPhyloPTrain.py --numProc 6 --noAncestors \
+	--no4d ${targetGenomeStr} ${halFile} ${srcOrg} ${4dSitesBed} $@.${tmpExt}.mod --error ${errorFile}
 	mv -f $@.${tmpExt}.mod $@
